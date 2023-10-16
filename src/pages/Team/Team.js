90b89b4635eb02f22ccf762a0f6dd292fa2ca4b4
SEERRGEE <ieg.sergo@gmail.com>
2023-10-16 16:33:35 -0400

    change team images

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -30,24 +30,31 @@ return (
         <section className='team__section'>
             <div className='team__container'>
                 <div className='team__member'>
-                    <img className='team__image' src={require('../../assets/images/worker1.png')}/>
+                {/* <div className='team__image team__image--kevin'>
+                </div> */}
+                <img className='team__image' src={require('../../assets/images/worker1.png')}/>
                     <div className='team__content'>
                         <h3 className='team__name'>Kevin Keene</h3>
-                        <p className='team__description'>Kevin is the backbone of Optimum Cabinets, bringing passion, dedication, and exceptional craftsmanship to every project. With years of experience, he has honed his skills to perfection, ensuring that every cabinet he touches is a work of art. Kevin's attention to detail and commitment to quality make him an invaluable member of our team, and his work speaks volumes about our commitment to excellence.</p>
+                        <p className='team__description'>Kevin has over 2,000,000 ft.² of residential construction experience and 25 years dedicated to cabinetry design and finishing. His experience and passion creates a unique one of a kind result</p>
                     </div>
                 </div>
                 <div className='team__member'>
-                    <img className='team__image' src={require('../../assets/images/worker2.png')}/>
+                <img className='team__image' src={require('../../assets/images/worker2.png')}/>
+                {/* <div className='team__image team__image--rodrigo'>
+                </div> */}
                     <div className='team__content'>
-                        <h3 className='team__name'>Rodrigo</h3>
-                        <p className='team__description'>Rodrigo is a true artisan when it comes to crafting cabinets at Optimum Cabinets. His meticulous approach and unwavering commitment to quality shine through in every piece he creates. With a passion for woodworking that is second to none, Rodrigo transforms raw materials into functional works of art. When you choose Optimum Cabinets, you're choosing Rodrigo's craftsmanship and dedication, ensuring your cabinets are nothing less than exceptional. Rodrigo embodies our commitment to delivering the finest quality, making him an integral part of our team.</p>
+                        <h3 className='team__name'>Rodrigo De Castro</h3>
+                        <p className='team__description'>Rodrigo de Castro, is a seasoned professional with over a decade in the construction industry. He specializes in creating exquisite custom cabinets and providing top-tier workmanship.
+Rodrigo thrives on pushing the boundaries of design, making him the ideal choice for discerning and high-end clients.  Rodrigo's artistry transforms your vision into reality, crafting opulent spaces. </p>
                     </div>
                 </div>
                 <div className='team__member'>
                     <img className='team__image' src={require('../../assets/images/worker3.png')}/>
+                {/* <div className='team__image team__image--renaldo'>
+                </div> */}
                     <div className='team__content'>
-                        <h3 className='team__name'>Serge</h3>
-                        <p className='team__description'>At Optimum Cabinets, Serge is a master of precision and detail. With years of experience in the industry, he brings a wealth of knowledge and expertise to our team. Serge's exceptional craftsmanship is evident in every cabinet he constructs. His dedication to perfection ensures that each piece meets the highest standards of quality. When you select Optimum Cabinets, you benefit from Serge's commitment to excellence, making your cabinet project a true work of art. Serge is an indispensable member of our team, embodying our unwavering commitment to delivering top-notch craftsmanship.</p>
+                        <h3 className='team__name'>Renaldo De Souza</h3>
+                        <p className='team__description'>With over 25 years of Woodworking and cabinet experience, Ronaldo is a critical part of our installation team, making sure that the final product is exemplary.</p>
                     </div>
                 </div>
             </div>
